cbd65a2cfdc8686531327bb3c0757a23e526a2fd
t3r6 <eugene.crm@outlook.com>
2018-02-24 15:59:43 +0200

    lesson 5 exercise 16

--- src/com/lesson5/Part16.java
+++ src/com/lesson5/Part16.java
@@ -8,17 +8,22 @@ package com.lesson5;
  * (внутренний цикл for должен перебирать элементы не с конца, а с начала).
  */
 
-public class Part16b {
+public class Part16 {
     public static void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
+            boolean flag = false;
             for (int j = 0; j < array.length - i - 1; j++) {
                 {
                     if (array[j] < array[j + 1]) {
                         int tmp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = tmp;
+                        flag = true;
                     }
                 }
+            }
+            if (!flag) {
+                break;
             }
         }
     }
