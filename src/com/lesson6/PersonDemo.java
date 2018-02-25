c4561353063db97111df47f75c262e8f02b31056
t3r6 <eugene.crm@outlook.com>
2018-02-25 18:16:23 +0200

    lesson 6 Person

--- src/com/lesson6/PersonDemo.java
+++ src/com/lesson6/PersonDemo.java
@@ -1,5 +1,14 @@
 package com.lesson6;
 
+/**
+ * Создать класс Person, который содержит:
+ * a) поля fullName, age.
+ * б) методы move() и talk(), в которых просто вывести на консоль сообщение -"Такой-то  Person говорит".
+ * в) Добавьте два конструктора  - Person() и Person(fullName, age).
+ * Создайте два объекта этого класса. Один объект инициализируется конструктором Person(),
+ * другой - Person(fullName, age).
+ */
+
 public class PersonDemo {
     public static void main(String[] args) {
         Person person1 = new Person();
