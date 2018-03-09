6dbc3d4ed22d31abbd7e5eb5ce471017fa6114e1
t3r6 <eugene.crm@outlook.com>
2018-03-09 11:35:19 +0200

    figures

---
+++ src/com/lesson9/part1/Pear.java
@@ -0,0 +1,58 @@
+package com.lesson9.part1;
+
+/**
+ * Фруктовая лавка. Создать абстрактный класс Фрукт и классы Яблоко, Груша, Абрикос расширяющие его.
+ * Класс Фрукт содержит:
+ * a) переменную вес,
+ * б) завершенный метод printManufacturerInfo(){System.out.print("Made in Ukraine");}
+ * в) абстрактный метод, возвращающий стоимость фрукта, который должен быть переопределен
+ * в каждом классе наследнике. Метод должен учитывать вес фрукта.
+ * Создать несколько объектов разных классов. Подсчитать общую стоимость проданных фруктов.
+ * А также общую стоимость отдельно проданных яблок, груш и абрикос.
+ */
+
+public class Pear extends Fruit{
+    public Pear(double weight, double price) {
+        super(weight, price);
+    }
+
+    @Override
+    public double getWeight() {
+        return super.getWeight();
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        super.setWeight(weight);
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public void setPrice(double price) {
+        super.setPrice(price);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public double cost(Fruit fruit) {
+        return (getWeight() * getPrice());
+    }
+}
---
+++ src/com/lesson9/part2/Rectangle.java
@@ -0,0 +1,67 @@
+package com.lesson9.part2;
+
+/**
+ * Создайте суперкласс Shape и его наследники Circle, Rectangle.
+ * Класс Shape содержит абстрактный метод draw() и переменную хранящую цвет.
+ * Классы Circle, Rectangle содержат координаты точек. Создать массив содержащий эти фигуры.
+ * В цикле нарисовать их (вызвать метод draw).
+ * Добавить метод equals()  для классов Shape, Circle, Rectangle. (Задание с урока)
+ */
+
+public class Rectangle extends Shape{
+    private int x, y;
+
+    public Rectangle(String color, int x, int y) {
+        super(color);
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void draw(){
+        System.out.println("Рисую прямоугольник "+ getColor() + " цвета");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Rectangle rectangle = (Rectangle) o;
+
+        if (x != rectangle.x) return false;
+        return y == rectangle.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "x=" + x +
+                ", y=" + y +
+                "} " + super.toString();
+    }
+}
