c90c83924600d839d7d075362d5bdd05c88fb1d0
t3r6 <eugene.crm@outlook.com>
2018-03-09 12:51:47 +0200

    circle

---
+++ src/com/lesson9/part3/Circle.java
@@ -0,0 +1,38 @@
+package com.lesson9.part3;
+
+/**
+ * Создать класс Circle, который содержит
+ * - переменную radius;
+ * - методы, вычисляющие площадь и длину окружности;
+ * - константу PI.
+ * Создать несколько объектов данного класса.
+ */
+
+public class Circle {
+    private double radius;
+    public static final double PI = 3.14;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public static double getPI() {
+        return PI;
+    }
+
+    public double area() {
+        return ((radius * radius) * PI);
+    }
+
+    public double circuit() {
+        return (2 * PI * radius);
+    }
+}
---
+++ src/com/lesson9/part3/CircleDemo.java
@@ -0,0 +1,30 @@
+package com.lesson9.part3;
+
+import java.text.*;
+
+/**
+ * Создать класс Circle, который содержит
+ * - переменную radius;
+ * - методы, вычисляющие площадь и длину окружности;
+ * - константу PI.
+ * Создать несколько объектов данного класса.
+ */
+
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(4);
+        Circle circle2 = new Circle(7);
+        Circle circle3 = new Circle(10);
+        Circle circle4 = new Circle(8);
+        Circle circle5 = new Circle(9);
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("Площадь первого круга " + circle1.area() +
+                ", Длина окружности " + circle1.circuit());
+        System.out.println("Площадь второго круга " + circle2.area() +
+                ", Длина окружности " + circle2.circuit());
+        System.out.println("Площадь третьего круга " + circle3.area() +
+                ", Длина окружности " + (df.format(circle3.circuit())));
+        System.out.println("Площадь четвёртого круга " + circle4.area() +
+                ", Длина окружности " + circle4.circuit());
+    }
+}
