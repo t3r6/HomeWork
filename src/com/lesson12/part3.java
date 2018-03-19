e9a9502a3a9967062215f43c97ad6dbf129a69a1
t3r6 <eugene.crm@outlook.com>
2018-03-19 16:25:45 +0200

    oop

---
+++ src/com/lesson12/part3.java
@@ -0,0 +1,17 @@
+package com.lesson12;
+
+/**
+ * Напишите метод, заменяющий в строке все вхождения «object oriented programming»
+ * (не учитываем регистр символов) на «OOP».
+ */
+
+public class part3 {
+    public static void main(String[] args) {
+        String s = "object oriented programming is difficult";
+        Replacement(s);
+    }
+
+    private static void Replacement(String s) {
+        System.out.println(s.replace("object oriented programming", "OOP"));
+    }
+}
