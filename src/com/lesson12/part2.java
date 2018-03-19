2dee63e865febab22b5f1edca4174ae552b32b7a
t3r6 <eugene.crm@outlook.com>
2018-03-19 15:43:58 +0200

    string builder

---
+++ src/com/lesson12/part2.java
@@ -0,0 +1,54 @@
+package com.lesson12;
+
+/**
+ * а) Дано два числа, например 3 и 56, необходимо составить следующие строки:
+ * 3 + 56 = 59
+ * 3 – 56 = -53
+ * 3 * 56 = 168.
+ * Используем метод StringBuilder.append().
+ * б) Замените символ “=” на слово “равно”. Используйте методы StringBuilder.insert(), StringBuilder.deleteCharAt().
+ * в) Замените символ “=” на слово “равно”. Используйте методы StringBuilder.replace(). (Задание с урока).
+ */
+
+public class part2 {
+    public static void main(String[] args) {
+        calculate(3, 56);
+        calculate1(4, 57);
+    }
+
+    private static void calculate(int a, int b) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(a).append(" + ").append(b).append(" = ").append(a + b).append("\n");
+        stringBuilder.append(a).append(" - ").append(b).append(" = ").append(a - b).append("\n");
+        stringBuilder.append(a).append(" * ").append(b).append(" = ").append(a * b).append("\n");
+        System.out.println(stringBuilder);
+
+        int pos = 0;
+        while ((pos = stringBuilder.indexOf("=")) != -1) {
+            stringBuilder.replace(pos, pos + 1, "равно");
+        }
+        System.out.println(stringBuilder);
+    }
+
+    public static void calculate1(int a, int b) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(a).append(" + ").append(b).append(" = ").append(a + b).append("\n");
+        stringBuilder.append(a).append(" - ").append(b).append(" = ").append(a - b).append("\n");
+        stringBuilder.append(a).append(" * ").append(b).append(" = ").append(a * b).append("\n");
+        System.out.println(stringBuilder);
+
+        int pos = 0;
+        while ((pos = stringBuilder.indexOf("=")) != -1) {
+            stringBuilder.deleteCharAt(pos);
+            stringBuilder.insert(pos, " равно ");
+        }
+        System.out.println(stringBuilder);
+    }
+}
+
+
+
+
+
