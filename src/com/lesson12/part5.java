b755c29e35e332260fc819aea2045dec5a5dde3d
t3r6 <eugene.crm@outlook.com>
2018-03-19 17:26:25 +0200

    formatting

--- src/com/lesson12/part5.java
+++ src/com/lesson12/part5.java
@@ -12,11 +12,15 @@ import java.util.Formatter;
 public class part5 {
     public static void main(String[] args) {
         printString("Петров", 5, "физике");
+        printString1("Иванов", 3, "математике");
     }
 
     public static void printString(String fullName, int mark, String subject) {
         Formatter formatter = new Formatter();
-        formatter.format("%15s получил оценку %3d по %10s", fullName, mark, subject);
+        formatter.format("%-15s получил оценку %3d по %10s", fullName, mark, subject);
         System.out.println(formatter);
+    }
+    public static void printString1(String fullName, int mark, String subject){
+        System.out.printf("%-15s получил оценку %3d по %10s", fullName, mark, subject);
     }
 }
