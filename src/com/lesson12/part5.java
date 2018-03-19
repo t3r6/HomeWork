1df1e2fe62824de18c743a267b66dc84d5daf1de
t3r6 <eugene.crm@outlook.com>
2018-03-19 17:19:28 +0200

    formatting

---
+++ src/com/lesson12/part5.java
@@ -0,0 +1,22 @@
+package com.lesson12;
+
+import java.util.Formatter;
+
+/**
+ * Создать строку, используя форматирование: Студент [Фамилия] получил [оценка] по [предмету].
+ * Форматирование и вывод строки на консоль написать в отдельном методе, который принимает фамилию,
+ * оценку и название предмета в качестве параметров.
+ * Выделить под фамилию 15 символов, под оценку 3 символа, предмет – 10. (Задание с урока).
+ */
+
+public class part5 {
+    public static void main(String[] args) {
+        printString("Петров", 5, "физике");
+    }
+
+    public static void printString(String fullName, int mark, String subject) {
+        Formatter formatter = new Formatter();
+        formatter.format("%15s получил оценку %3d по %10s", fullName, mark, subject);
+        System.out.println(formatter);
+    }
+}
