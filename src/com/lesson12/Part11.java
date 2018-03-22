afcb2cd4d2a2fa740449a8dd8b6673836c1bbfeb
t3r6 <eugene.crm@outlook.com>
2018-03-22 16:27:51 +0200

    minChars

--- src/com/lesson12/Part11.java
+++ src/com/lesson12/Part11.java
@@ -11,6 +11,31 @@ import java.util.regex.Pattern;
 
 public class Part11 {
     public static void main(String[] args) {
+        String str = "fffff ab f 1234 jkjk";
+        System.out.print("Заданная строка: " + str + "\nСлово с минимальным количеством различных символов: ");
+        minChars(str);
+    }
 
+    private static void minChars(String str) {
+        String[] min = str.split(" ");
+        StringBuilder s = new StringBuilder();
+        String text;
+        int ch1, ch2 = str.length();
+        for (String word : str.split(" ")) {
+            ch1 = 0;
+            s.setLength(0);
+            for (String string : word.split("")) {
+                text = String.valueOf(string);
+                if (s.indexOf(text) == -1) {
+                    s.append(text);
+                    ch1++;
+                }
+            }
+            if (ch1 < ch2) {
+                ch2 = ch1;
+                min[0] = word;
+            }
+        }
+        System.out.println(min[0]);
     }
 }
