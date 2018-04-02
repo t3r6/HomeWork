fd03b505fe5212c93d451ca94009f4844fab6be1
t3r6 <eugene.crm@outlook.com>
2018-04-02 17:43:23 +0300

    boxes

---
+++ src/com/lesson16/Part3.java
@@ -0,0 +1,9 @@
+package com.lesson16;
+
+/**
+ * Создать TreeSet содержащий HeavyBox. HeavyBox должен реализовать интерфейс Comparable.
+ * Распечатать содержимое с помощью for each. (Задание с урока)
+ */
+
+public class Part3 {
+}
