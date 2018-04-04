443f57397f80add2c61b914cadf543c8b4d432f0
t3r6 <eugene.crm@outlook.com>
2018-04-04 14:13:56 +0300

    lesson16 part7

---
+++ src/com/lesson16/Part7.java
@@ -0,0 +1,24 @@
+package com.lesson16;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Создать очередь, содержащую объекты класса HeavyBox.
+ * Используем класс ArrayDeque. Поместить объекты в очередь с помощью метода offer().
+ * Удалить объекты методом poll().  (Задание с урока)
+ */
+
+public class Part7 {
+    public static void main(String[] args) {
+        Deque<HeavyBox1> queue = new ArrayDeque<>();
+        HeavyBox1 box = new HeavyBox1(2, 2, 2, 2);
+        queue.offer(new HeavyBox1(1, 1, 1, 1));
+        queue.offer(new HeavyBox1(2, 2, 2, 2));
+        queue.offer(box);
+
+        while (queue.size() != 0) {
+            System.out.println(queue.poll());
+        }
+    }
+}
