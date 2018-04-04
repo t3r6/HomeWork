4341c6139caf23e2b715776087647d35f9a30f9f
t3r6 <eugene.crm@outlook.com>
2018-04-04 16:52:58 +0300

    lesson16 part4

--- src/com/lesson16/Part4.java
+++ src/com/lesson16/Part4.java
@@ -1,5 +1,9 @@
 package com.lesson16;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Напишите методы union(Set<?>... set) и intersect(Set<?> ... set),
  * реализующих операции объединения и пересечения множеств.
@@ -7,4 +11,43 @@ package com.lesson16;
  */
 
 public class Part4 {
+    public static void main(String[] args) {
+        Set<HeavyBox1> boxes1 = new TreeSet<>();
+        Set<HeavyBox1> boxes2 = new TreeSet<>();
+        Set<HeavyBox1> boxes3 = new TreeSet<>();
+
+
+        boxes1.add(new HeavyBox1(1, 1, 1, 1));
+        boxes1.add(new HeavyBox1(2, 2, 2, 2));
+        boxes1.add(new HeavyBox1(3, 3, 3, 3));
+
+        boxes2.add(new HeavyBox1(1, 1, 1, 1));
+        boxes2.add(new HeavyBox1(5, 5, 5, 5));
+        boxes2.add(new HeavyBox1(3, 3, 3, 3));
+
+        boxes3.add(new HeavyBox1(7, 7, 7, 7));
+        boxes3.add(new HeavyBox1(2, 2, 2, 2));
+        boxes3.add(new HeavyBox1(3, 3, 3, 3));
+
+        union(boxes1, boxes2, boxes3);
+        intersect(boxes1, boxes2);
+        intersect(boxes1, boxes3);
+    }
+
+    public static void union(Set<?>... set) {
+        TreeSet<?> result = new TreeSet<>();
+        for (Set<?> s : set) {
+            result.addAll((Collection) s);
+        }
+        System.out.println(result);
+        ;
+    }
+
+    public static void intersect(Set<?>... set) {
+        TreeSet<?> result = (TreeSet) set[1];
+        for (Set<?> s : set) {
+            result.retainAll(s);
+        }
+        System.out.println(result);
+    }
 }
