36446d06345e406ac3a5fb21ec66245a46ab0ec3
t3r6 <eugene.crm@outlook.com>
2018-04-06 22:53:24 +0300

    lesson16 fixes

--- src/com/lesson16/Part13.java
+++
@@ -1,9 +0,0 @@
-package com.lesson16;
-
-/**
- * Переделать задание 4 (класс Товар),  используя Collections.sort() для сортировки.
- * Добавить возможность сортировать в обратном порядке (Collections.reverse()).
- */
-
-public class Part13 {
-}
